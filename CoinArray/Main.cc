de3c7ea5b5572508c0c657235828d8ed4efd0936
Oscar Espejo Sagués <xxgreysoulxx@gmail.com>
2017-03-25 23:09:14 +0100

    añado colores :D

--- CoinArray/Main.cc
+++ CoinArray/Main.cc
@@ -2,6 +2,7 @@
 #include <string>
 #include <ctime>
 #include <conio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "Map.hh"
 #include "Player.hh"
@@ -16,10 +17,12 @@ int menu() {
 
 	int diff;
 
+	system("color 8B");
 	std::cout << "//////////////////////////////" << std::endl;
 	std::cout << "          COIN RACE           " << std::endl;
 	std::cout << "//////////////////////////////" << std::endl;
 	std::cout << endl;
+
 	std::cout << "Selecciona una de las 3 dificultades" << std::endl;
 	std::cout << endl;
 	std::cout << "Easy == 1" << std::endl;
--- CoinArray/Map.cc
+++ CoinArray/Map.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <ctime>
+#include <stdio.h>
 #include <conio.h>
 #include "Map.hh"
 
