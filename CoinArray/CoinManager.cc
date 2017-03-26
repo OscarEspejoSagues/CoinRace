84d4f3de40c8c5b7f883ae0a899f46226b326d8c
Oscar Espejo Sagués <xxgreysoulxx@gmail.com>
2017-03-26 16:17:15 +0200

    Codigo comentado falta rematar las monedas

--- CoinArray/CoinManager.cc
+++ CoinArray/CoinManager.cc
@@ -4,25 +4,25 @@
 #include <conio.h>
 #include "CoinManager.hh"
 
-CoinManager::CoinManager(int &rows, int &colus) {
+CoinManager::CoinManager(int &rows, int &colus) {            //Constructor de la clase coinManager
 	filasMapa = rows;
 	columnasMapa = colus;
 }
 
-int CoinManager::coininmap(apuntCoins **total) const {
+int CoinManager::coininmap(apuntCoins **total) const {      //Ponemos todas las monedas en el mapa
 	*total = coinData;
 	return cantidaddemonedas;
 }
 
-int CoinManager::monedas(int rows,int columns) {
+int CoinManager::monedas(int rows,int columns) {			//Generamos el numero de monedas
 	return (rand() % (rows*columns / 10)) + rows*columns*0.03;
 }
 
-void CoinManager::coinGenerator(int rows, int columns) {
+void CoinManager::coinGenerator(int rows, int columns) {    //Reposicionamos las monedas de manera que no se repita la posicion de ninguna de ellas
 	
 	cantidaddemonedas = monedas(rows, columns);
-	coinData = static_cast<apuntCoins*>(malloc(cantidaddemonedas * sizeof(apuntCoins)));
-	int randX, randY;
+	coinData = static_cast<apuntCoins*>(malloc(cantidaddemonedas * sizeof(apuntCoins)));  //Usando malloc, podemos reservar un espacio de memoria sin necessidad de
+	int randX, randY;                                                                    //poner un valor en el
 	bool repeated;
 	for (int i = 0; i < cantidaddemonedas; i++) {
 		do {
@@ -40,7 +40,7 @@ void CoinManager::coinGenerator(int rows, int columns) {
 	}
 };
 
-void CoinManager::removeCoin(int &posX, int &posY) {
+void CoinManager::removeCoin(int &posX, int &posY) {  //Metodo para eliminar las monedas una vez acabado
 	int help = -1;
 }
 
